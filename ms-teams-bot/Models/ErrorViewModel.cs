ffc5706e398299bb0526958272890b4ae2fd094a
Dillon Nys <dnys@brwncald.com>
2019-12-28 10:58:52 -0700

    Add project files.

---
+++ ms-teams-bot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ms_teams_bot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
